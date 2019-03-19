b2dcb645bf6d4f9c1f2ca943aa40bcb8ec3ba6ec
Grzegorz Kuleszyński <grzegorz.kuleszynski@gmail.com>
2019-03-19 15:44:06 +0100

    StartUp

--- src/main/java/com/learnspring/LearnApp/HelloWorldClass.java
+++ src/main/java/com/learnspring/LearnApp/HelloWorldClass.java
@@ -1,13 +1,15 @@
 package com.learnspring.LearnApp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloWorldClass {
 
     @RequestMapping("/")
-    public String index(){
+    public @ResponseBody
+    String getHelloWorld(){
         return "Hello World";
     }
 }
--- src/test/java/com/learnspring/LearnApp/LearnAppApplicationTests.java
+++ src/test/java/com/learnspring/LearnApp/LearnAppApplicationTests.java
@@ -2,15 +2,25 @@ package com.learnspring.LearnApp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LearnAppApplicationTests {
 
+	@Autowired
+	private HelloWorldClass hello;
+
 	@Test
-	public void contextLoads() {
+	public void test() {
+
+		assertEquals("Hello World",hello.getHelloWorld());
+
+
 	}
 
 }
